41fddc47044c78ad8c5b37a5a6a2011b77bcf839
nguyentduc89 <nguyentduc89@gmail.com>
2025-12-31 18:19:12 +0700

    Add files via upload

---
+++ src/main/java/com/vti/cicd2504/Cicd2504Application.java
@@ -0,0 +1,13 @@
+package com.vti.cicd2504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd2504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd2504Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vti/cicd2504/Cicd2504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.cicd2504;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2504ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
